23ecbe9c28bc9dbc0c3ef30f7fd2bd7f0d762d10
aditi <neil.sukalikar@gmail.com>
2024-09-21 22:34:36 +0200

    google analytics code is added

--- client-app/pages/_document.tsx
+++ client-app/pages/_document.tsx
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MVJBDXF7KD"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MVJBDXF7KD', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
